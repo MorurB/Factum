cb08b45b9ff3fb51e68e867160281f5bd24de809
MorurB <118909455+MorurB@users.noreply.github.com>
2022-11-26 16:15:13 +0200

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
-let burger = document.querySelector('.burger');
-let menu = document.querySelector('.menu');
-let body = document.querySelector('.body');
-
-burger.addEventListener('click', function () {
-    burger.classList.toggle('burger_active');
-    body.classList.toggle('body_active');
-    burger_menu.classList.toggle('burger-menu_active');
-})
